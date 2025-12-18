d3c5a5afb17328c877260f153cbedf2709eeeb82
Vincent <magicdroidx@gmail.com>
2025-12-18 19:59:37 +0000

    build: update test scripts

--- v8_test.sh
+++ v8_test.sh
@@ -1,20 +1,30 @@
 #!/bin/sh
-
 set -e
 
 dir="$(cd "$(dirname "$0")" && pwd)"
 
 if [ ! -d "${dir}/v8" ]; then
-	echo "v8 not found"
-	exit 1
+  echo "v8 not found"
+  exit 1
+fi
+
+cxx="g++"
+cxxflags=""
+ldflags=""
+
+if [ -n "${LLVM_VERSION:-}" ]; then
+  cxx="clang++-${LLVM_VERSION}"
+  cxxflags="${cxxflags} -stdlib=libc++"
+  ldflags="${ldflags} -stdlib=libc++"
 fi
 
 (
-	set -x
-	g++ -I"${dir}/v8" -I"${dir}/v8/include" \
-		"${dir}/v8/samples/hello-world.cc" -o hello_world \
-		-lv8_monolith -L"${dir}/v8/out/release/obj/" \
-		-pthread -std=c++17 -ldl
+  set -x
+  "${cxx}" -I"${dir}/v8" -I"${dir}/v8/include" \
+    "${dir}/v8/samples/hello-world.cc" -o hello_world \
+    -L"${dir}/v8/out/release/obj/" -lv8_monolith \
+    -pthread -std=c++17 -ldl \
+    ${cxxflags} ${ldflags}
 )
 
 sh -c "./hello_world"
